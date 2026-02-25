023852fc9dbe23d770310cff15ce5d3fe6e74147
zhengjiaqi03 <zhengjiaqi03@kuaishou.com>
2026-02-25 16:23:23 +0800

    思考字段更新

--- p2r_agents/config.py
+++ p2r_agents/config.py
@@ -9,7 +9,7 @@ from openai import (
 
 BASE_URL = os.environ.get("SILICONFLOW_BASE_URL", "https://api.siliconflow.cn/v1")
 API_KEY = os.environ.get("SILICONFLOW_API_KEY", "sk-nhnklopejonbklumkchlnsjaluxbetocvqdzevgcrjptjlpj")
-TEXT_MODEL = "deepseek-ai/DeepSeek-V3.2"
+TEXT_MODEL = "Pro/deepseek-ai/DeepSeek-V3.2"
 # TEXT_MODEL = "Qwen/Qwen3-VL-32B-Thinking"
 
 base_client = OpenAI(
@@ -24,12 +24,14 @@ def base_dpsk_settings(
     top_k: int = 20,
     thinking: bool = False,
 ):
+    think_type = "enabled" if thinking else "disabled"
     return {
         "max_tokens": max_tokens,
         "temperature": temperature,
         "top_p": top_p,
         "extra_body": {
             "top_k": top_k,
-            "chat_template_kwargs": {"thinking": thinking},
+            # "chat_template_kwargs": {"thinking": thinking},
+            "thinking": {"type": think_type} 
         }
     }
